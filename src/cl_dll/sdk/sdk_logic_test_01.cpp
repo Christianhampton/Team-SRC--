b03809c1fece2757af95ed649aad9429fcaefc01
Ian <newsuperyoshi@yahoo.com>
2013-03-27 22:38:16 -0500

    added src and license

---
+++ src/cl_dll/sdk/sdk_logic_test_01.cpp
@@ -0,0 +1,26 @@
+/*
+ Copyright 2013 Team SRC++
+  All Rights Reserved
+
+ Comments from Dev:
+
+                    */
+#include "sdk_logic_test_01.h"
+#include "cbase.h"
+#include "c_base.h"
+
+class ClogicalEntityTestOne
+{
+public:
+    DECLARE_CLASS (ClogicalEntityTestOne);
+    //easter egg for devs.
+};
+sdk_logic_test_01::sdk_logic_test_01()
+{
+    //ctor
+}
+
+sdk_logic_test_01::~sdk_logic_test_01()
+{
+    //dtor
+}
---
+++ src/cl_dll/sdk/sdk_logic_test_01.h
@@ -0,0 +1,24 @@
+/*
+  Copyright 2013 Team SRC++
+   All Rights Reserved
+
+  Comments from Dev:
+
+                     */
+#ifndef SDK_LOGIC_TEST_01_H
+#define SDK_LOGIC_TEST_01_H
+#include <cbase.h>
+
+
+class sdk_logic_test_01
+{
+    public:
+        /** Default constructor */
+        sdk_logic_test_01();
+        /** Default destructor */
+        virtual ~sdk_logic_test_01();
+    protected:
+    private:
+};
+
+#endif // SDK_LOGIC_TEST_01_H
